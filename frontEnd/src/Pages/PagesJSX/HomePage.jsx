5b467267b4e0b5a56e29da44f0d446bbd7596288
Mona Lisa <talal.alrafee@gmail.com~>
2024-11-04 21:11:27 +0300

    smallAdjument

--- frontEnd/src/Pages/PagesJSX/HomePage.jsx
+++ frontEnd/src/Pages/PagesJSX/HomePage.jsx
@@ -3,21 +3,25 @@ import React  from 'react'
 import Header from "../../Components/Header/Header.jsx";
 import '../PagesCSS/homepage.css'
 // import Blocker  from "../../assets/block-1.svg"
-import GreetingBlock from '../../Components/GreetingBlock/GreetingBlock.jsx';
+import InteractionBlock from '../../Components/InteractionBlock/InteractionBlock.jsx';
 import FeatureCards from '../../Components/FeatureCards/FeatureCards.jsx';
+import Panel from "../../Components/Panel/Panel.jsx"
+import BrainGiver from "../../assets/images/BrainGiver.svg"
 function HomePage() {
   
 
   return (<div id='homepage'>
     <Header/>
-   <GreetingBlock title="See what people think in a glance" description="Sentiment Scout delivers real-time insights from youtube, twitter and Google maps. <br />
-                        Whether you are a content creater, buisness owner, or someone curiuos about public opinions."/>
+   <InteractionBlock center={true} title="See what people think in a glance" description={<>Sentiment Scout delivers real-time insights from youtube, twitter and Google maps. <br />
+                        Whether you are a content creater, buisness owner, or someone curiuos about public opinions.</>}buttonWidth="20%" buttonHeight= "15.01%"
+                       marginBottom="20%" marginTop="20.86vh" width="45.16%" height="46.04vh" fontSize="85px" />
    <FeatureCards />
+   <Panel src={BrainGiver}/>
     
 
   </div>
 
-   
+
   )
 }
 
