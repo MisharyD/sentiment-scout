2099369c2b6567c97bb745b2f3b794f720fc0131
Mona Lisa <talal.alrafee@gmail.com~>
2024-11-05 22:57:45 +0300

    homepage

--- frontEnd/src/Components/AuthSideBar/AuthSideBar.jsx
+++ frontEnd/src/Components/AuthSideBar/AuthSideBar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "./auth-sidebar.css";
+import AuthSideBarImage from "../../assets/images/AuthSideBarImage2.svg"
 function AuthSideBar() {
   return (
     <div id="auth-sidebar">
+      <img className="auth-sidebar-image" src={AuthSideBarImage}></img>
     <svg
   viewBox="0 0 500 500"
   xmlns="http://www.w3.org/2000/svg"
